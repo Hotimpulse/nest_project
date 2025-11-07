a77a718b97b3e8cc58bae38480c36912f246b8dd
Hotimpulse <hotimpulse@hotmail.com>
2025-11-07 22:53:06 +0300

    feat: adding auth guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
     return this.generateToken(user);
   }
 
-  private async generateToken(user: User) {
+  private generateToken(user: User) {
     const payload = { email: user.email, id: user.id, roles: user.roles };
     return {
       token: this.jwtService.sign(payload),
