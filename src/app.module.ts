f680a2979b60d89b43f84dd7a208e8d6220467b6
Hotimpulse <hotimpulse@hotmail.com>
2025-11-07 16:02:24 +0300

    feat: adding a user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: String(process.env.POSTGRES_HOST),
+      port: Number(process.env.POSTGRES_PORT),
+      username: String(process.env.POSTGRES_USER),
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: String(process.env.POSTGRES_DB),
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.model.ts
@@ -0,0 +1,20 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationsAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationsAttrs> {
+  @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
+  declare id: number;
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  declare email: string;
+  @Column({ type: DataType.STRING, allowNull: false })
+  declare password: string;
+  @Column({ type: DataType.BOOLEAN, defaultValue: false })
+  declare banned: boolean;
+  @Column({ type: DataType.STRING, allowNull: true })
+  declare banReason: string;
+}
